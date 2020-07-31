f5a68eb4bc277f010959ba64d13eb498983ad7da
alexgeenen <geenen124@gmail.com>
2020-07-31 23:10:30 +0200

    Update OG and Twitter Info Index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -38,14 +38,14 @@ export default {
         { property: "og:title", content: this.$static.metadata.siteName },
         { property: "og:description", content: this.$static.metadata.siteDescription },
         { property: "og:url", content: this.$static.metadata.siteUrl },
-        { property: "og:image", content: this.ogImageUrl },
+        // { property: "og:image", content: this.ogImageUrl },
 
         { name: "twitter:card", content: "summary_large_image" },
         { name: "twitter:title", content: this.$static.metadata.siteName },
         { name: "twitter:description", content: this.$static.metadata.siteDescription },
-        { name: "twitter:site", content: "@cossssmin" },
-        { name: "twitter:creator", content: "@cossssmin" },
-        { name: "twitter:image", content: this.ogImageUrl },
+        { name: "twitter:site", content: "@geenen124" },
+        { name: "twitter:creator", content: "@geenen124" },
+        // { name: "twitter:image", content: this.ogImageUrl },
       ],
     }
   },
